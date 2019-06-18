63a230ccf1339cc4c081e89cbe6a5feecbce8af4
Samantha Kane <s.kane133@gmail.com>
2019-06-18 17:24:44 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -41,7 +41,7 @@ end
 
 def shortened_tweet_truncator(words)
   if word_substituter(words) > 140
-    word_substituter(words)[0...140]
+    word_substituter(words)[0...140] << "..."
   else
     words
   end
