ef64650e0401960ec03b0e497c69fc9c185d8549
Samantha Kane <s.kane133@gmail.com>
2019-06-18 17:07:48 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -2,6 +2,14 @@
 def dictionary(hash)
   dictionary = {
     
-  
+   "too" => "2",
+  "to" => "2",
+  "two" =>"2",
+  "four" => "4",
+  "for" => "4",
+  "be" => "b",
+  "you" => "u",
+  "at" => "@",
+  "and" => "&"
   }
 end 
