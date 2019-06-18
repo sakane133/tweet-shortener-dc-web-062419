a61a553619c5a2c50036777fb0c8c6e5788c8a29
Samantha Kane <s.kane133@gmail.com>
2019-06-18 17:01:18 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,10 +1,4 @@
 # Write your code here.
 word_substituter(string)
-string.split.map do |words|
-  if dictionary.include?(word)
-    dictionary[word]
-  else 
-    word
-  end 
- end.join("")
-end
+
+end 
